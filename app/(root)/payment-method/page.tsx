4486f9cf03f2c9aa1e7e9bb0227e0d4dcaf6c172
webapp07 <aminezouguig@proton.me>
2026-04-27 15:51:13 -0400

    Payment method page

---
+++ app/(root)/payment-method/page.tsx
@@ -0,0 +1,24 @@
+import { auth } from "@/auth";
+import { getUserById } from "@/lib/actions/user.actions";
+import PaymentMethodForm from "./payment-method-form";
+
+export const metadata = {
+  title: "Payment Method",
+  description: "Manage your payment methods and preferences.",
+};
+
+const PaymentMethodPage = async () => {
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <PaymentMethodForm preferredPaymentMethod={user!.paymentMethod} />
+    </>
+  );
+};
+export default PaymentMethodPage;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -74,19 +74,17 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
 }
 
 // Get user by ID
+
 export async function getUserById(userId: string) {
   try {
-    const user = await prisma.user.findFirst({
+    const user = await prisma.user.findUnique({
       where: { id: userId },
     });
-    if (!user) {
-      throw new Error("User not found");
-    }
 
     return user;
   } catch (error) {
-    console.error("Error fetching user:", error);
-    return null;
+    console.error("Database error fetching user:", error);
+    throw new Error("Failed to fetch user");
   }
 }
 
