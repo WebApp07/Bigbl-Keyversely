5442ee3181dbe3b1e588f2b2f41e0364d07f377f
webapp07 <aminezouguig@proton.me>
2026-04-29 19:57:30 -0400

    fix Broken Link

--- app/(root)/place-order/page.tsx
+++ app/(root)/place-order/page.tsx
@@ -93,7 +93,7 @@ const PlaceOrderPage = async () => {
                     <TableRow key={item.slug}>
                       <TableCell>
                         <Link
-                          href={`/product/item.slug`}
+                          href={`/product/${item.slug}`}
                           className="flex items-center"
                         >
                           <Image
