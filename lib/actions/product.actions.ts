e99624886090e244ed387597244424ceb94f8fe4
webapp07 <aminezouguig@proton.me>
2026-05-22 11:15:40 -0400

    I went back to display products for admin page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -2,7 +2,6 @@
 
 import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
-import { PAGE_SIZE } from "../constants";
 
 // Create a connection to the database using Prisma Client
 //const prisma = new PrismaClient();
@@ -37,30 +36,4 @@ export async function getProductBySlug(slug: string) {
     // Throw a safe generic message
     throw new Error("Could not load product");
   }
-}
-
-// Get all products
-
-export async function getAllProducts({
-  query,
-  limit = PAGE_SIZE,
-  page,
-  category,
-}: {
-  query: string;
-  limit?: number;
-  page: number;
-  category?: string;
-}) {
-  const data = await prisma.product.findMany({
-    skip: (page - 1) * limit,
-    take: limit,
-  });
-
-  const dataCount = await prisma.product.count();
-
-  return {
-    data,
-    totalPages: Math.ceil(dataCount / limit),
-  };
 }
