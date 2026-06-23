f996124fbd18a5fba5192bd6c098a9d5c01a7c13
WebApp07 <aminezouguig@proton.me>
2026-06-23 15:46:58 +0200

    fix parameter item place order page

--- app/(root)/place-order/page.tsx
+++ app/(root)/place-order/page.tsx
@@ -13,7 +13,7 @@ import {
 import { getMyCart } from "@/lib/actions/cart.actions";
 import { getUserById } from "@/lib/actions/user.actions";
 import { formatCurrency } from "@/lib/utils";
-import { ShippingAddress } from "@/types";
+import { CartItem, ShippingAddress } from "@/types";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
@@ -90,7 +90,7 @@ const PlaceOrderPage = async () => {
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  {cart.items.map((item) => (
+                  {cart.items.map((item: CartItem) => (
                     <TableRow key={item.slug}>
                       <TableCell>
                         <Link
