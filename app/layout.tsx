6bc8d84ff66b65d1b3a125f626437eb527d5392b
WebApp07 <aminezouguig@proton.me>
2026-06-25 17:41:23 +0200

    add metadata layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template: `%s | Bigbl`,
+    template: `%s | keyversely`,
     default: APP_NAME,
   },
   description: APP_DESCRIPTION,
